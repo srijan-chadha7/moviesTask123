6d0b8b0d53aa3441062df99f447d4a85f1a65a7c
srijan-chadha7 <srijan.chadha14@gmail.com>
2022-02-26 21:08:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./footer";
+
+
+const HomePage = React.lazy(() => import("./components/HomePage"));
+const SingleMoviePage = React.lazy(() => import("./components/singlePage"));
+function App() {
+  
+  return <>
+   <Router>
+      <Navbar />
+      <React.Suspense fallback={"loading  ....."}>
+        <Switch>
+          <Route path="/" exact component={HomePage}  />
+          <Route path="/movies/:id" component={SingleMoviePage} />
+          
+            
+        </Switch>
+      </React.Suspense>
+    </Router>
+    <Footer/>
+  </>
+   
+    
+  
+}
+
+export default App;
